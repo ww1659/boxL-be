81d80c3e88100e4149a028333dec319ecddcbecd
Billy White <billywhite@Billys-Mac-mini.local>
2024-01-16 13:32:28 +0000

    updated connection.js

--- db/connection.js
+++ db/connection.js
@@ -15,4 +15,6 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set");
 }
 
+console.log(process.env.DATABASE_URL);
+
 module.exports = new Pool(config);
