ee5f4a4bf4085a895eea8d52162edbfc2c98ef0c
Billy White <billywhite@Billys-Mac-mini.local>
2024-01-16 10:32:35 +0000

    attempting to seed online db

--- db/connection.js
+++ db/connection.js
@@ -11,6 +11,9 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+console.log("Resolved path for dotenv:", `${__dirname}/../.env.${ENV}`);
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set");
 }
