882391738622e3f8b5979c6b88c14fa828a926a7
Billy White <billywhite@Billys-Mac-mini.local>
2024-02-02 15:37:35 +0000

    updated endpoints controller

--- controllers/endpoints-controller.js
+++ controllers/endpoints-controller.js
@@ -1,6 +1,5 @@
 const endPoints = require("../endpoints.json");
 
 exports.getEndpoints = (req, res, next) => {
-  delete endPoints[Object.keys(endPoints)[0]];
   res.status(200).send({ endPoints });
 };
